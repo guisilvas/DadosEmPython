48a8db69f2802e7f0162cbe9d8bdcdfbbac2226f
guisilvas <guilherme7soaress@gmail.com>
2024-01-22 09:27:36 -0300

    Update Aula01

--- test.py
+++ test.py
@@ -63,3 +63,35 @@
 #     listDouble.append(item*2)
 # print(listDouble)
 
+# listGrades = []
+
+# Problema gerador: Criar uma calculadora em Python
+respostaSim = ["sim", "Sim", "SIM", "s", "S", "siM", "sIm", "sIM", "SIm", "SiM"]
+respostaNao = ["nao", "Nao", "NAO", "n", "N", "naO", "nAo", "nAO", "NAo", "NaO", "não", "Não", "NãO", "NÃo"]
+comecar = ""
+comecar = input("\n    Instruções Calculadora Python*    \n\nPara calcular digite primeiro a operação que deseja realizar e depois dois números. Após a primeira operação o resultado fica salvo, você pode zerá-lo com a opção de numeral '0' ou continuar calculando selecionando uma operação. Entendeu?\n\nDigite: sim ou nao: ")
+while (comecar):
+    if (comecar == respostaSim[i]):
+        print("Ok")
+    elif (comecar == respostaNao[i]):
+        print("Leia atentamente as instruções!")
+        break
+
+
+
+
+# while (comecar != "sim") or (comecar != "Sim") or (comecar != "SIM")or (comecar != "S") or (comecar != "s") or (comecar != "SIm") or (comecar != "SiM") or (comecar != "sIM") or (comecar != "sIM"):
+#     if (comecar == "sim") or (comecar == "Sim") or (comecar == "SIM")or (comecar == "S") or (comecar == "s") or (comecar == "SIm") or (comecar == "SiM") or (comecar == "sIM") or (comecar == "sIM"):
+#         i = 0
+#         print("~"*25)
+#         print("  Calculadora em Python")
+#         print("~"*25)
+#         print("Operações:\n  0- Zerar\n  1-  Soma\n  2-  Subtração\n  3-  Multiplicação\n  4-  Divisão\n  5-  Exponenciação\n  6-  Resto\n  7-  Parte inteira\n  8-  Paridade\n  9-  Módulo\n 10-  Sair")
+#         print("~"*25)
+#         while i > 5:
+#             if (i == 1):
+#                 print("oi")
+#     elif (comecar == "nao") or (comecar == "Nao") or (comecar == "NAO")or (comecar == "N") or (comecar == "n") or (comecar == "Nao") or (comecar == "NaO") or (comecar == "nAO") or (comecar == "nAo") or (comecar == "não") or (comecar == "Não") or (comecar == "NÂO"):
+#         print("Leia atentamente as instruções!")
+#     else:
+#         print("Insira uma resposta válida.")
